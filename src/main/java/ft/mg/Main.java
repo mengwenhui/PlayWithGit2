c6cd3b8c75c07630b2201d09c5b5e4ab82f0c68c
mengwenhui <mengwenhui@gmail.com>
2023-12-27 11:19:52 -0800

    2nd changes

--- src/main/java/ft/mg/Main.java
+++
@@ -1,7 +0,0 @@
-package ft.mg;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/ft/mg/PlayIt.java
@@ -0,0 +1,7 @@
+package ft.mg;
+
+public class PlayIt {
+    public static void main(String[] args) {
+        System.out.println("Hello world! Play it with GitHub");
+    }
+}
