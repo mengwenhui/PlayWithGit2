647e7c272b17823f80fccd4e6639045a3e09a4b2
mengwenhui <mengwenhui@gmail.com>
2023-12-27 12:35:41 -0800

    changes from feature1 branch

---
+++ src/main/java/ft/mg/PlayIt.java
@@ -0,0 +1,8 @@
+package ft.mg;
+
+public class PlayIt {
+    public static void main(String[] args) {
+        System.out.println("Hello world! Play it with GitHub");
+        System.out.println("Add another output for feature1 branch");
+    }
+}
